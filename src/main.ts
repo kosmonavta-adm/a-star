11cde2e834ea3baa2009ed245c84679e2633261e
Krzysztof <krzadamczak@gmail.com>
2023-09-11 21:58:02 +0200

    Add grid

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,41 @@
-import { Application, Container } from "pixi.js";
+import { Application, Graphics, Container } from "pixi.js";
+
+import { BLOCK_SIZE } from "./constants";
+
+function drawGrid() {
+    const container = new Container();
+
+    const horizontalLines = new Graphics();
+    const verticalLines = new Graphics();
+
+    const { innerWidth, innerHeight } = window;
+
+    const horizontalLinesCount = innerWidth / BLOCK_SIZE;
+    const verticalLinesCount = innerHeight / BLOCK_SIZE;
+
+    let x = 0;
+    let y = 0;
+
+    for (let i = 0; i < horizontalLinesCount; i++) {
+        horizontalLines.moveTo(x, 0);
+        horizontalLines.lineStyle(1, "#ffffff", 0.1);
+        horizontalLines.lineTo(x, innerHeight);
+        horizontalLines.closePath();
+        x += BLOCK_SIZE;
+    }
+
+    for (let i = 0; i < verticalLinesCount; i++) {
+        verticalLines.moveTo(0, y);
+        verticalLines.lineStyle(1, "#ffffff", 0.1);
+        verticalLines.lineTo(innerWidth, y);
+        verticalLines.closePath();
+        y += BLOCK_SIZE;
+    }
+
+    container.addChild(horizontalLines, verticalLines);
+
+    return container;
+}
 
 const app = new Application<HTMLCanvasElement>({
     background: "#1099bb",
@@ -8,5 +45,8 @@ const app = new Application<HTMLCanvasElement>({
 const container = new Container();
 
 app.stage.addChild(container);
+
+const grid = drawGrid();
+container.addChild(grid);
 
 document.body.appendChild(app.view);
